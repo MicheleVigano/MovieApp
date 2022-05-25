0844574b85c5ed449c9e8aa8f2ec3cfb5eb6c970
Michele <michele.vigano.work@gmail.com>
2022-05-25 12:03:31 +0200

    +'type' search

--- js/module-api.js
+++ js/module-api.js
@@ -1,10 +1,20 @@
-const URL_LIST_TERMINATOR = "http://www.omdbapi.com/?apikey=ca90f062&t=terminator";
+const URL_LIST_TERMINATOR = "http://www.omdbapi.com/?apikey=ca90f062&s=terminator";
+const URL_TV_TERMINATOR = "http://www.omdbapi.com/?apikey=ca90f062&s=terminator&type=series"
 
 export const listTerminator = () => {
     fetch(URL_LIST_TERMINATOR)
     .then((response) => response.json())
     .then((results) => {
-        const films = result.Search;
-        console.log("films");
+        const films = results.Search;
+        console.log(films);
+    });
+};
+
+export const tvTerminator = () => {
+    fetch(URL_TV_TERMINATOR)
+    .then((response) => response.json())
+    .then((results) => {
+        const films = results.Search;
+        console.log(films);
     });
 };
